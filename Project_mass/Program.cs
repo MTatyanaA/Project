3ec299a494cf7ab2cc5ed77717f5c4d551f94cd2
Tatyana <turina_tatyana@mail.ru>
2022-08-13 22:23:31 +0300

    project_mass

---
+++ Project_mass/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+ {
+      static void Main(string[] args)
+    int [] numbers = {1, 3 , 4, 546, 67 ,4};
+        Console.WriteLine(numbers[0]);  // 1
+        // 0 - 1 1 - 3
+        int len = numbers.Length;
+       Console.WriteLine(numbers[numbers.Length-1])
+     }
