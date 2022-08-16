2ce6fd26f9f818b69624dc39374ce6e42d8f8ba2
Tatyana <turina_tatyana@mail.ru>
2022-08-16 17:28:49 +0300

    Igra3

---
+++ Igra3seminar/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Новая_папка
+{
+    
+    class Program
+    {
+        static int DO(int index)
+        {
+            return index * index;
+        }
+        static void Main(string[] args)
+        {
+            // && - и
+            // || - или
+            int a = 15;
+            int b = 20;
+            int c = 12;
+            int x = 0;
+            bool flag = a > b;
+            int index = flag ? DO(a): DO(b);
+            Console.WriteLine(index);
+            if((a > b && a > c) || (a == x)) // false true
+            {
+
+            }
+        }
+    }
+}
