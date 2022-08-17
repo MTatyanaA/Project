6e7652603cde685bfa153bb98bbbb2c162768715
Tatyana <turina_tatyana@mail.ru>
2022-08-17 14:44:08 +0300

    Formuly

---
+++ Formuly/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Новая_папка
+{ 
+    class Program
+    {
+       
+        static void Main(string[] args)
+     {
+        while (true)
+        {
+             int number = Convert.ToInt32(Console.ReadLine());
+             Console.WriteLine(Math.Abs(number));  // Модуль
+             Console.WriteLine(Math.Sqrt(number));  // Корень числа
+             Console.WriteLine(Math.Round(Math.Sqrt(number), 1));  // Целое
+             Console.WriteLine(Math.Pow(number, 10));  //Степень
+             Console.WriteLine("y/n");
+             if(Console.ReadLine() == "n") break;
+        }
+        
+     }   
+        }
+    }
+
